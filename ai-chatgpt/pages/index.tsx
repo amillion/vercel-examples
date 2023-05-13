e9f16ed8f3458f3eaa9d6674ec10dcef5417e8e4
Hanno <80357793+HannoCoetzee@users.noreply.github.com>
2023-05-13 13:55:15 +1000

    Update index.tsx
    Change some text

--- ai-chatgpt/pages/index.tsx
+++ ai-chatgpt/pages/index.tsx
@@ -5,9 +5,9 @@ function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <Text variant="h1">ChatGPT-style Chat Bot with Memory</Text>
+        <Text variant="h1">FinLyte - Making finance personal</Text>
         <Text className="text-zinc-600">
-          Each user session has their own persistent memory.
+          Personal financial advice for everyone!
         </Text>
       </section>
 
