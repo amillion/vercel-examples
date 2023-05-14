d52556eb6fc1ab548de15d28c5d1965f92d8f52e
Hanno <80357793+HannoCoetzee@users.noreply.github.com>
2023-05-14 11:59:22 +1000

    Update Nav.tsx

--- ai-chatgpt/components/Nav.tsx
+++ ai-chatgpt/components/Nav.tsx
@@ -19,7 +19,11 @@ export default function Nav({ path, deployButton }: NavProps) {
 
   return (
     <nav className="border-b border-gray-200 py-5 relative z-20 bg-background shadow-[0_0_15px_0_rgb(0,0,0,0.1)]">
-      <img src="./public/FinLyte.svg" />
+      <div className="flex items-center lg:px-6 px-8 mx-auto max-w-7xl px-14">
+        <div className="flex flex-row items-center">
+          <Logo className={`w-8 h-8 text-black`} ></Logo>
+        </div>
+      </div>
     </nav>
   )
 }
