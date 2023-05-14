1818601f1846fee212af3b2c04e92bbf6a21b99b
Hanno <80357793+HannoCoetzee@users.noreply.github.com>
2023-05-14 12:12:14 +1000

    Update Nav.tsx

--- ai-chatgpt/components/Nav.tsx
+++ ai-chatgpt/components/Nav.tsx
@@ -2,9 +2,6 @@ import Link from '@vercel/examples-ui/link'
 import Button from '@vercel/examples-ui/button'
 import DeployButton, { DeployButtonProps } from '@vercel/examples-ui/deploy-button'
 import { Layout, Text, Page } from '@vercel/examples-ui'
-import Logo from './Steamship'
-
-const REPO_URL = 'https://github.com/steamship-core/vercel-examples/tree/main'
 
 export interface NavProps {
   path: string
